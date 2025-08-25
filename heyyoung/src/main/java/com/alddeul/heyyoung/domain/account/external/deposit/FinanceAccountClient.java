71635de8c25203274a0e80d589d453902c194414
YeongJoon <60257302+Yjooon@users.noreply.github.com>
2025-08-25 22:34:36 +0900

    feat(account): SSAFY API 입출금 구현
    저번에 올린 파일 디렉토리 구조 변경으로 인해 삭제되어서, 다시 올렸습니다.
    transactionBalance type이 엑셀은 double인데 매뉴얼 명세서는 long이여서, long으로 일단 적었습니다.

--- heyyoung/src/main/java/com/alddeul/heyyoung/domain/account/external/deposit/FinanceAccountClient.java
+++ heyyoung/src/main/java/com/alddeul/heyyoung/domain/account/external/deposit/FinanceAccountClient.java
@@ -1,4 +1,62 @@
 package com.alddeul.heyyoung.domain.account.external.deposit;
 
+
+import com.alddeul.heyyoung.common.api.external.dto.FinanceApiResponse;
+import com.alddeul.heyyoung.common.api.external.dto.RequestHeader;
+import com.alddeul.heyyoung.common.api.external.utils.WebClientHelper;
+import com.alddeul.heyyoung.domain.account.external.deposit.dto.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
 public class FinanceAccountClient {
+    private final WebClientHelper webClientHelper;
+
+    @Value("${financial.api.base-url}")
+    private String url;
+
+    @Value("${financial.api.key}")
+    private String appKey;
+
+    @Value("${financial.api.default.account-type}")
+    private String defaultAccountType;
+
+
+    public FinanceApiResponse<CreateAccountResponse> createDemandDepositAccount(String userKey) {
+        String apiName = "createDemandDepositAccount";
+        RequestHeader header = RequestHeader.of(apiName, appKey, userKey);
+        CreateAccountRequest request = CreateAccountRequest.of(header, defaultAccountType);
+        return webClientHelper.postRequest(url + "/api/v1/edu/demandDeposit/createDemandDepositAccount", request, CreateAccountResponse.class);
+    }
+
+    public FinanceApiResponse<InquireAccountListResponse> inquireDemandDepositAccountList(String userKey) {
+        String apiName = "inquireDemandDepositAccountList";
+        RequestHeader header = RequestHeader.of(apiName, appKey, userKey);
+        InquireAccountListRequest request = InquireAccountListRequest.of(header);
+        return webClientHelper.postRequest(url + "/api/v1/edu/demandDeposit/inquireDemandDepositAccountList", request, InquireAccountListResponse.class);
+    }
+
+    public FinanceApiResponse<InquireAccountHistoryResponse> inquireTransactionHistory(String userKey, String accountNo, Long transactionNo) {
+        String apiName = "inquireTransactionHistory";
+        RequestHeader header = RequestHeader.of(apiName, appKey, userKey);
+        InquireAccountHistoryRequest request = InquireAccountHistoryRequest.of(header, accountNo, transactionNo);
+        return webClientHelper.postRequest(url + "/api/v1/edu/demandDeposit/inquireTransactionHistory", request, InquireAccountHistoryResponse.class);
+    }
+
+    public FinanceApiResponse<WithdrawalAccountResponse> withdrawDemandDepositAccount(String userKey, String accountNo, Long transactionBalance, String transactionSummary) {
+        String apiName = "withdrawDemandDepositAccount";
+        RequestHeader header = RequestHeader.of(apiName, appKey, userKey);
+        WithdrawalAccountRequest request = WithdrawalAccountRequest.of(header, accountNo, transactionBalance, transactionSummary);
+        return webClientHelper.postRequest(url + "/api/v1/edu/demandDeposit/updateDemandDepositAccountWithdrawal", request, WithdrawalAccountResponse.class);
+    }
+
+    public FinanceApiResponse<DepositAccountResponse> depositDemandDepositAccount(String userKey, String accountNo, Long transactionBalance, String transactionSummary) {
+        String apiName = "depositDemandDepositAccount";
+        RequestHeader header = RequestHeader.of(apiName, appKey, userKey);
+        DepositAccountRequest request = DepositAccountRequest.of(header, accountNo, transactionBalance, transactionSummary);
+        return webClientHelper.postRequest(url + "/api/v1/edu/demandDeposit/updateDemandDepositAccountDeposit", request, DepositAccountResponse.class);
+    }
 }
---
+++ heyyoung/src/main/java/com/alddeul/heyyoung/domain/account/external/deposit/dto/WithdrawalAccountResponse.java
@@ -0,0 +1,22 @@
+package com.alddeul.heyyoung.domain.account.external.deposit.dto;
+
+import com.alddeul.heyyoung.common.api.external.dto.ResponseHeader;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+public class WithdrawalAccountResponse {
+    @JsonProperty("Header")
+    private ResponseHeader header;
+    @JsonProperty("REC")
+    private List<Rec> rec;
+
+    @Getter
+    @NoArgsConstructor
+    public static class Rec {
+        private Long transactionUniqueNo;
+        private String transactionDate;
+    }
+}

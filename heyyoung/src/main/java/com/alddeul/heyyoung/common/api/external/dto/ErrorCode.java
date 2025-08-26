16fdc08b102bd0bd49147a78268b6d68c0f6def3
YeongJoon <60257302+Yjooon@users.noreply.github.com>
2025-08-26 19:12:02 +0900

    feat(common): errorCode 추가

--- heyyoung/src/main/java/com/alddeul/heyyoung/common/api/external/dto/ErrorCode.java
+++ heyyoung/src/main/java/com/alddeul/heyyoung/common/api/external/dto/ErrorCode.java
@@ -1,7 +1,14 @@
 package com.alddeul.heyyoung.common.api.external.dto;
 
 public enum ErrorCode {
-    E4003("존재하지 않는 회원입니다");
+    E4003("존재하지 않는 회원입니다"),
+
+    // 기관거래고유번호가 중복된 값 -> 이미 처리된 요청으로 신뢰
+    H1007("이미 거래된 요청입니다."),
+
+    A1014("계좌 잔액이 부족하여 거래가 실패했습니다."),
+    A1003("계좌번호가 유효하지 않습니다."),
+    A1011("거래 금액이 유효하지 않습니다.");
 
     private final String message;
 

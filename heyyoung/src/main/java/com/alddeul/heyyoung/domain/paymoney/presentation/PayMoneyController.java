8ae2af666ba4f99f8be42f3de85231a8d3e790f1
YeongJoon <60257302+Yjooon@users.noreply.github.com>
2025-08-28 00:26:52 +0900

    fix(paymoney): 응답 형식 ApiResponse로 변경

--- heyyoung/src/main/java/com/alddeul/heyyoung/domain/paymoney/presentation/PayMoneyController.java
+++ heyyoung/src/main/java/com/alddeul/heyyoung/domain/paymoney/presentation/PayMoneyController.java
@@ -1,10 +1,10 @@
 package com.alddeul.heyyoung.domain.paymoney.presentation;
 
+import com.alddeul.heyyoung.common.api.response.ApiResponse;
 import com.alddeul.heyyoung.domain.paymoney.application.PayMoneyService;
 import com.alddeul.heyyoung.domain.paymoney.presentation.request.PayMoneyTransactionRequest;
 import com.alddeul.heyyoung.domain.paymoney.presentation.response.PayMoneyTransactionResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,14 +14,14 @@ public class PayMoneyController {
     private final PayMoneyService payMoneyService;
 
     @PostMapping("/topup")
-    public ResponseEntity<PayMoneyTransactionResponse> topUpPayMoney(@RequestBody PayMoneyTransactionRequest payMoneyTransactionRequest) {
+    public ApiResponse<PayMoneyTransactionResponse> topUpPayMoney(@RequestBody PayMoneyTransactionRequest payMoneyTransactionRequest) {
         PayMoneyTransactionResponse response = payMoneyService.requestTopUpPayMoney(payMoneyTransactionRequest);
-        return ResponseEntity.ok().body(response);
+        return ApiResponse.ok(response);
     }
 
     @PostMapping("/refund")
-    public ResponseEntity<PayMoneyTransactionResponse> refund(@RequestBody PayMoneyTransactionRequest payMoneyTransactionRequest) {
+    public ApiResponse<PayMoneyTransactionResponse> refund(@RequestBody PayMoneyTransactionRequest payMoneyTransactionRequest) {
         PayMoneyTransactionResponse response = payMoneyService.requestRefundPayMoney(payMoneyTransactionRequest);
-        return ResponseEntity.ok().body(response);
+        return ApiResponse.ok(response);
     }
 }

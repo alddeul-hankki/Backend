7bc63650462bf9edf53a6c4d99e7ccd7867a742a
daabaat <lllllls189@gmail.com>
2025-08-27 22:14:04 +0900

    feat(campus) : Campus 관련 Entity 작성

--- solsolhanhankki/src/main/java/com/alddeul/solsolhanhankki/campus/model/entity/Campus.java
+++ solsolhanhankki/src/main/java/com/alddeul/solsolhanhankki/campus/model/entity/Campus.java
@@ -1,26 +1,21 @@
 package com.alddeul.solsolhanhankki.campus.model.entity;
 
 import com.alddeul.solsolhanhankki.common.jpa.base.entity.BaseIdentityEntity;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import java.math.BigDecimal;
 
 @Entity
-@Table(name = "pickup_zones")
+@Table(name = "campuses")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class PickupZone extends BaseIdentityEntity {
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "campus_id", nullable = false)
-    private Campus campus;
-
-    @Column(nullable = false)
+public class Campus extends BaseIdentityEntity {
+    @Column(nullable = false, unique = true)
     private String name;
-
-    private String description;
 
     @Column(nullable = false, precision = 10, scale = 8)
     private BigDecimal latitude;

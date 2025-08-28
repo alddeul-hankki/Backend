ff5cb98c8549ea32e2990ca2d2aff1cba01c0464
daabaat <lllllls189@gmail.com>
2025-08-29 02:25:49 +0900

    feat(group) : 그룹주문 관련 repository 추가

---
+++ solsolhanhankki/src/main/java/com/alddeul/solsolhanhankki/order/model/repository/OrderRepository.java
@@ -0,0 +1,18 @@
+package com.alddeul.solsolhanhankki.order.model.repository;
+
+import com.alddeul.solsolhanhankki.order.model.entity.Orders;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Orders, Long> {
+
+    Optional<Orders> findById(Long orderId);
+
+    List<Orders> findAllByGroupId(Long groupId);
+
+    Optional<Orders> findByIdAndUserId(Long orderId, Long userId);
+}

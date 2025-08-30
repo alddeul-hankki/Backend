2209f84dc00b5949e846cb57d02f6b3fc467a448
jinyeong212 <ar61208@gmail.com>
2025-08-30 09:46:16 +0900

    feat:chage url

--- ddangyo/src/main/java/com/alddle/ddangyo/restaurant/controller/RestaurantController.java
+++ ddangyo/src/main/java/com/alddle/ddangyo/restaurant/controller/RestaurantController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/ddangyo/api")
 @RequiredArgsConstructor
 public class RestaurantController {
 
